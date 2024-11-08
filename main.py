3c8f06e9d574c3a0accc1446b53a18a505533fc9
James Bostock <James.Bostock@checkmarx.com>
2024-11-08 11:33:23 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Change 1
+# Change 2
+# Change 3
+# Change 4
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/execute', methods=['POST'])
+def execute_user_code_unsafe():
+    user_code = request.args.get('code')
+    result = eval(user_code)
+    return result['value']
+
+@app.route('/execute-test', methods=['POST'])
+def execute_user_code_unsafe_test():
+    user_code = request.args.get('code')
+    result = eval(user_code)
+    return result['value']
